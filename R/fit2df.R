2a6f7ffad8621030ede22f14d722f920a5620744
ewenharrison <mail@ewenharrison.com>
2019-05-17 16:19:56 +0100

    Website

--- R/fit2df.R
+++ R/fit2df.R
@@ -52,8 +52,8 @@
 #' explanatory = c("age.factor", "sex.factor", "obstruct.factor", "perfor.factor")
 #' dependent = "mort_5yr"
 #' colon_s %>%
-#' 	glmmulti(dependent, explanatory) %>%
-#' 	fit2df(estimate_suffix=" (univariable)")
+#'   glmmulti(dependent, explanatory) %>%
+#'   fit2df(estimate_suffix=" (univariable)")
 #'
 #' # glmerMod
 #' explanatory = c("age.factor", "sex.factor", "obstruct.factor", "perfor.factor")
@@ -61,15 +61,15 @@
 #' dependent = "mort_5yr"
 #' colon_s %>%
 #'   glmmixed(dependent, explanatory, random_effect) %>%
-#' 	 fit2df(estimate_suffix=" (multilevel)")
+#'   fit2df(estimate_suffix=" (multilevel)")
 #'
 #' # glmboot
 #' ## Note number of draws set to 100 just for speed in this example
 #' explanatory = c("age.factor", "sex.factor", "obstruct.factor", "perfor.factor")
 #' dependent = "mort_5yr"
 #' colon_s %>%
-#' 	glmmulti_boot(dependent, explanatory,  R = 100) %>%
-#' 	fit2df(estimate_suffix=" (multivariable (BS CIs))")
+#'   glmmulti_boot(dependent, explanatory,  R = 100) %>%
+#'   fit2df(estimate_suffix=" (multivariable (BS CIs))")
 #'
 #' # lm
 #' fit = lm(nodes ~  age.factor + sex.factor + obstruct.factor + perfor.factor,
@@ -84,26 +84,42 @@
 #'
 #' colon_s %>%
 #'   lmmixed(dependent, explanatory, random_effect) %>%
-#' 	 fit2df(estimate_suffix=" (multilevel")
+#'   fit2df(estimate_suffix=" (multilevel")
 #'
 #' # coxphlist
 #' explanatory = c("age.factor", "sex.factor", "obstruct.factor", "perfor.factor")
 #' dependent = "Surv(time, status)"
 #'
 #' colon_s %>%
-#' 	coxphuni(dependent, explanatory) %>%
-#' 	fit2df(estimate_suffix=" (univariable)")
+#'   coxphuni(dependent, explanatory) %>%
+#'   fit2df(estimate_suffix=" (univariable)")
 #'
 #' colon_s %>%
-#' 	coxphmulti(dependent, explanatory) %>%
-#' 	fit2df(estimate_suffix=" (multivariable)")
+#'   coxphmulti(dependent, explanatory) %>%
+#'   fit2df(estimate_suffix=" (multivariable)")
 #'
 #' # coxph
 #' fit = coxph(Surv(time, status) ~ age.factor + sex.factor + obstruct.factor + perfor.factor,
 #'   data = colon_s)
 #'
 #' fit %>%
-#' 	fit2df(estimate_suffix=" (multivariable)")
+#'   fit2df(estimate_suffix=" (multivariable)")
+#' 	
+#' # crr: competing risks
+#' library(cmprsk)
+#' library(magrittr)
+#' 	
+#' # Simulated data from package 
+#' set.seed(10)
+#' ftime = rexp(200)
+#' fstatus = sample(0:2, 200, replace=TRUE)
+#' cov = matrix(runif(600), nrow=200)
+#' dimnames(cov)[[2]] = c('var1','var2','var3')
+#' df = data.frame(ftime, fstatus, cov)
+#' 	
+#' df %$% 
+#'   crr(ftime, fstatus, cov) %>%
+#'   fit2df()
 
 fit2df <- function(...){
 	UseMethod("fit2df")
