dbd7e1da4a819ad5ea08dfa24df00ed96eaaa2d2
ewenharrison <mail@ewenharrison.com>
2019-10-24 19:12:34 +0100

    #25

--- R/ff_remove_ref.R
+++ R/ff_remove_ref.R
@@ -6,6 +6,8 @@
 #' \code{\link{or_plot}}, see below.
 #'
 #' @param .data Output from \code{\link{finalfit}} or similar.
+#' @param only_binary Logical. Remove reference level only for two-level
+#'   factors. When set to false, reference level for all factors removed.
 #'
 #' @return Data frame.
 #' @export
@@ -18,29 +20,38 @@
 #' 	 finalfit(dependent, explanatory, add_dependent_label = FALSE) %>%
 #' 	 ff_remove_ref() %>%
 #' 	 dependent_label(colon_s, dependent)
-#' 	
+#'
 #' # Plot example
 #' explanatory = c("age.factor", "age", "sex.factor", "nodes", "obstruct.factor", "perfor.factor")
 #' dependent = 'mort_5yr'
-#' colon_s %>% 
-#'   summary_factorlist(dependent, explanatory, total_col = TRUE, fit_id=TRUE) %>% 
+#' colon_s %>%
+#'   summary_factorlist(dependent, explanatory, total_col = TRUE, fit_id=TRUE) %>%
 #'   ff_merge(
-#'     glmuni(colon_s, dependent, explanatory) %>% 
-#'     fit2df()) %>% 
-#'   ff_remove_ref() %>% 
+#'     glmuni(colon_s, dependent, explanatory) %>%
+#'     fit2df()) %>%
+#'   ff_remove_ref() %>%
 #'   dplyr::select(-`OR`) -> factorlist_plot
 #'
-#' colon_s %>% 
+#' colon_s %>%
 #'    or_plot(dependent, explanatory, factorlist = factorlist_plot)
-ff_remove_ref <- function(.data){
+ff_remove_ref <- function(.data, only_binary = TRUE){
 	if(!any(names(.data) == "label")) stop("finalfit function must include: add_dependent_label = FALSE")
 	# estimate_col = grep("Coefficient*|OR*|HR*", names(.data), value=TRUE)[1]
-	.data %>% 
+	df.out = .data %>% 
 		dplyr::mutate(label = ifelse(label == "", NA, label)) %>% 
 		tidyr::fill(label) %>% 
-		dplyr::group_by(label) %>% 
-		# dplyr::filter_at(dplyr::vars(estimate_col), dplyr::any_vars(. != "-")) %>% 
-		dplyr::filter(levels %in% c("Mean (SD)", "Median (IQR)") | dplyr::row_number() != 1) %>% 
+		dplyr::group_by(label)
+	if(only_binary){
+		df.out = df.out %>% 
+			dplyr::filter(levels %in% c("Mean (SD)", "Median (IQR)") | 
+											dplyr::row_number() != 1 |
+											dplyr::n() > 2)
+	} else {
+		df.out = df.out %>% 
+			dplyr::filter(levels %in% c("Mean (SD)", "Median (IQR)") | 
+											dplyr::row_number() != 1)
+	}
+	df.out %>% 
 		as.data.frame() %>% 
 		rm_duplicate_labels()
 }
@@ -93,8 +104,8 @@ finalfit_remove_p = ff_remove_p
 ff_percent_only <- function(.data){
 	.data %>% 
 		dplyr::mutate_at(-c(1,2), ~ dplyr::case_when(!levels %in% c("Mean (SD)", "Median (IQR)") ~ 
-																						stringr::str_extract(., "(?<=\\().+?(?=\\))"), 
-																					TRUE ~ .))
+																								 	stringr::str_extract(., "(?<=\\().+?(?=\\))"), 
+																								 TRUE ~ .))
 }
 
 #' @rdname ff_percent_only
